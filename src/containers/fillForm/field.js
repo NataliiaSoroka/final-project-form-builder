8746cbd17d0cd32c34f090dbbcf7b1e92fae5a91
Nataliia Soroka <nataliiasoroka.gm@gmail.com>
2019-09-25 10:33:24 +0300

    add validation on fill form

--- src/containers/fillForm/field.js
+++ src/containers/fillForm/field.js
@@ -1,83 +1,89 @@
-import React from 'react';
-import { 
-    TextField, 
-    Checkbox, 
-    FormGroup,
-    FormControlLabel,
-    Grid,
-    MenuItem
-}  from '@material-ui/core';
+import React from 'react'
+import {
+  TextField,
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+  Grid,
+  MenuItem
+} from '@material-ui/core'
 
 function switchField(input, handlerChange) {
-    switch(input.type) {
-        case 'text':
-            return <TextField 
-                label={input.label} 
-                type={input.type} 
+  switch (input.type) {
+    case 'text':
+      return (
+        <TextField
+          label={input.label}
+          type={input.type}
+          name={input.name}
+          onChange={e => handlerChange(e.target.value)}
+          fullWidth
+          placeholder={input.placeholder}
+        />
+      )
+    case 'number':
+      return (
+        <TextField
+          label={input.label}
+          type={input.type}
+          name={input.name}
+          onChange={e => handlerChange(e.target.value)}
+          fullWidth
+          placeholder={input.placeholder}
+        />
+      )
+    case 'dropdown':
+      return (
+        <TextField
+          name={input.name}
+          label={input.label}
+          value={(input.items[input.default] || {}).value}
+          onChange={e => handlerChange(e.target.value)}
+          fullWidth
+          select>
+          {input.items.map(op => (
+            <MenuItem value={op.value} key={op.value}>
+              {op.name}
+            </MenuItem>
+          ))}
+        </TextField>
+      )
+    case 'checkmark':
+      return (
+        <FormGroup row>
+          <FormControlLabel
+            control={
+              <Checkbox
                 name={input.name}
-                onChange={(e) => handlerChange(e.target.value)}
-                fullWidth
-                required
-            />
-        case 'number':
-            return <TextField
-                label={input.label}
-                type={input.type}
-                name={input.name}
-                onChange={(e) => handlerChange(e.target.value)}
-                fullWidth
-                required
-            />
-        case "dropdown":
-            return <TextField
-                name={input.name}
-                label={input.label}
-                value={(input.items[input.default]|| {}).value}
-                onChange={(e) => handlerChange(e.target.value)}
-                fullWidth
-                required
-                select
-                >
-                    {input.items.map(op => <MenuItem value={op.value} key={op.value}>{op.name}</MenuItem>)}
-                </TextField>
-        case "checkmark":
-            return <FormGroup row>
-                <FormControlLabel
-                    control={
-                        <Checkbox 
-                            name={input.name}
-                            value={input.name}
-                            onChange={() => {
-                                const value = document.getElementsByName(input.name)[0].checked;                                handlerChange(value)
-                            }}
-                        />
-                    }
-                    label={input.label}
-                />
-            </FormGroup>
-        default: {
-            return null
-        }
+                value={input.name}
+                onChange={() => {
+                  const value = document.getElementsByName(input.name)[0]
+                    .checked
+                  handlerChange(value)
+                }}
+              />
+            }
+            label={input.label}
+          />
+        </FormGroup>
+      )
+    default: {
+      return null
     }
+  }
 }
 
 function Field(props) {
-    const { input, handlerChange} = props;
-    return (
-        <div>
-            <Grid 
-                container 
-                spacing={3}
-                justify="center"
-                alignItems="center"
-            >
-                <Grid item xs={12}>
-                    { switchField(input, handlerChange) }
-                </Grid>
-            </Grid>
-        </div>
-        
-    )
+  const { input, handlerChange } = props
+  return (
+    <div>
+      <Grid container spacing={3} justify="center" alignItems="center">
+        <Grid item xs={12}>
+          {SwitchField(input, handlerChange)}
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
-export default Field;
+export default Field
--- src/containers/fillForm/form.js
+++ src/containers/fillForm/form.js
@@ -1,72 +1,149 @@
-import React from 'react';
-import Field from './field';
-import { setfillToForm } from '../../modules/fills/thunks';
-import { Button, Card, CardHeader, CardContent, CardActions } from '@material-ui/core/';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/styles';
+import React from 'react'
+import Field from './field'
+import { setfillToForm } from '../../modules/fills/thunks'
+import { setMessage, setStatePopup, setStatus } from '../../modules/shared/actions';
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions
+} from '@material-ui/core/'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/styles'
+import { number, string, boolean, object } from 'yup'
+import { isNil, path } from 'ramda'
 
-const styles ={
-    card: {
-        minWidth: 500
-    }
+const styles = {
+  card: {
+    minWidth: 500
+  }
 }
 
 class Form extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state =  {
-            formValues: {}
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      formValues: {}
     }
-    handlerChange(name) {
-        return value => {
-            this.setState({
-                formValues: {
-                    ...this.state.formValues,
-                    [name]: value
-                }
-            })
+  }
+
+  componentDidMount() {
+    this.generateDefaultValues()
+  }
+
+  generateDefaultValues() {
+    const fields = this.props.form.fields || []
+    const formValues = fields.reduce((form, field) => {
+      const defaultValuesMap = {
+        checkmark: false,
+        text: '',
+        dropdown: path(['items', field.default, 'value'], field)
+      }
+      const defaultVal = defaultValuesMap[field.type]
+      return isNil(defaultVal) ? form : { ...form, [field.name]: defaultVal }
+    }, {})
+    this.setState({ formValues })
+  }
+
+  handlerChange(name) {
+    return value => {
+      this.setState({
+        formValues: {
+          ...this.state.formValues,
+          [name]: value
         }
+      })
     }
-    saveForm(){
-        const id = this.props.form.id
+  }
+
+  saveForm() {
+    const schema = this.createValidationSchema()
+    const {
+        setMessage,
+        setStatePopup,
+        setStatus,
+        form: { id }
+      } = this.props
+
+    schema
+      .validate(this.state.formValues)
+      .then(formValues => {
         const payload = {
-            fields: this.state.formValues
+            fields: formValues
         }
         this.props.setfillToForm(id, payload)
-    }
+      })
+      .catch(err => {
+        setMessage(err.message)
+        setStatePopup(true)
+        setStatus(true)
+      })
 
-    render() {
-        const { fields, name } = this.props.form
-        const { classes } = this.props
-        return (
-            <Card className={classes.card}>
-                <form>
-                    <CardHeader title={name} />
-                    <CardContent>
-                        { 
-                            fields.map(row => <Field input={row} key={row.name} handlerChange={this.handlerChange(row.name)}/>)
-                        }
-                    </CardContent>
-                    <CardActions>
-                        <Button color="primary" onClick={() => this.saveForm()}>Save form</Button>
-                    </CardActions>
-                </form>
-            </Card>
-        )
+  }
+
+  createValidationSchema() {
+    const fields = this.props.form.fields || []
+    const getValidator = type => {
+      const validatorsTypeMap = {
+        text: string().required(),
+        number: number().required(),
+        checkmark: boolean(),
+        dropdown: string().required()
+      }
+      return validatorsTypeMap[type]
     }
+    const shape = fields.reduce(
+      (s, field) => ({
+        ...s,
+        [field.name]: getValidator(field.type)
+      }),
+      {}
+    )
+    return object().shape(shape)
+  }
+
+  render() {
+    const formData = this.props.form || {}
+    const { fields = [], name = '' } = formData
+    const { classes } = this.props
+    return (
+      <Card className={classes.card}>
+        <form>
+          <CardHeader title={name} />
+          <CardContent>
+            {fields.map(row => (
+              <Field
+                input={row}
+                key={row.name}
+                handlerChange={this.handlerChange(row.name)}
+              />
+            ))}
+          </CardContent>
+          <CardActions>
+            <Button color="primary" onClick={() => this.saveForm()}>
+              Save form
+            </Button>
+          </CardActions>
+        </form>
+      </Card>
+    )
+  }
 }
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      setfillToForm
+      setfillToForm,
+      setMessage,
+      setStatePopup,
+      setStatus
     },
     dispatch
   )
 
-  export default connect(
-    null,
-    mapDispatchToProps
-  )(withStyles(styles)(Form))
+export default connect(
+  null,
+  mapDispatchToProps
+)(withStyles(styles)(Form))
